78709ad5a8a400d47382b1a1c7040b5ed540824d
chenjidong <chenjidong@xunlei.com>
2017-04-01 15:59:35 +0800

    Initial Commit

---
+++ JDCFFPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JDCFFPlayer
+//
+//  Created by maochengrui on 01/04/2017.
+//  Copyright © 2017 jidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JDCFFPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JDCFFPlayer
+//
+//  Created by maochengrui on 01/04/2017.
+//  Copyright © 2017 jidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
